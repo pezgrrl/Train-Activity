3f9c95f489eb91d816d79fde2247cad90674c1e6
Jill Morimoto <jillmorimoto@gmail.com>
2018-07-20 13:06:21 -0700

    final

---
+++ assets/javascript/app.js
@@ -0,0 +1,80 @@
+var config = {
+  apiKey: "AIzaSyBxydwxzGgy28Divmi2ik1i6CPO--UCiME",
+  authDomain: "fir-01-a7616.firebaseapp.com",
+  databaseURL: "https://fir-01-a7616.firebaseio.com",
+  projectId: "fir-01-a7616",
+  storageBucket: "fir-01-a7616.appspot.com",
+  messagingSenderId: "729272718789"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+function getValues(event){
+	event.preventDefault();
+
+  var trainName = $("#train").val().trim();
+  var destination = $("#destination").val().trim();
+  var startTrain = $("#arrival").val().trim();
+  var frequency = $("#freq").val().trim();
+
+
+  console.log(startTrain);
+
+
+
+
+  database.ref().push({
+    name: trainName,
+    route: destination,
+    freqTime: frequency,
+    firstTrain: startTrain
+  });
+
+
+  $("#train").val("");
+  $("#destination").val("");
+  $("#frequency").val("");
+  $("#arrival").val("");
+};
+
+database.ref().on("child_added",function(snapshot){
+  
+
+
+  var trainLine = snapshot.val().name;
+  var trainRoute = snapshot.val().route;
+  var trainFreq = snapshot.val().freqTime;
+  var trainArrival = snapshot.val().firstTrain;
+
+
+
+  var firstTrainPretty = moment(trainArrival, "hh:mm").subtract(1, "years");
+  var currentTime = moment();
+  var diffTime = moment().diff(moment(firstTrainPretty), "minutes");
+  var tRemainder = diffTime % trainFreq;
+  var tMinutesTillTrain = trainFreq - tRemainder;
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  
+
+
+
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainLine),
+    $("<td>").text(trainRoute),
+    $("<td>").text(trainFreq),
+    $("<td>").text(trainArrival),
+    $("<td>").text(tMinutesTillTrain),
+    
+
+  );
+
+
+  $("#train-table > tbody").append(newRow);
+});
+
+
+
+
+
+
